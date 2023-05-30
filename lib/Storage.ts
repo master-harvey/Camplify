1e5585131ba5abb4fde4a7e0455b7fc1baf02dc2
Master__Harvey <ianpadraicharvey@gmail.com>
2023-05-30 01:17:38 -0600

    no point in storage without auth

--- lib/Storage.ts
+++ lib/Storage.ts
@@ -5,7 +5,7 @@ import { AuthVals } from './Auth';
 // Construct Inputs
 export interface StorageProps extends NestedStackProps {
   appName: string,
-  auth?: AuthVals,
+  auth: AuthVals,
   cors?: [s3.CorsRule],
   allowedOrigins?: [string]
 }
@@ -36,80 +36,80 @@ export class Storage extends NestedStack {
       ]
     })
 
-    if (props.auth) { //https://docs.amplify.aws/lib/storage/getting-started/q/platform/js/#using-amazon-s3
-      props.auth.identityPool.authenticatedRole.attachInlinePolicy(new iam.Policy(this, "authPolicy", {
-        statements: [
-          new iam.PolicyStatement({
-            actions: [
-              's3:GetObject',
-              's3:PutObject',
-              's3:DeleteObject',
-            ],
-            resources: [
-              bucket.bucketArn + '/public/*',
-              bucket.bucketArn + '/protected/${cognito-identity.amazonaws.com:sub}/*',
-              bucket.bucketArn + '/private/${cognito-identity.amazonaws.com:sub}/*',
-            ],
-            effect: iam.Effect.ALLOW,
-          }),
-          new iam.PolicyStatement({
-            actions: ['s3:PutObject'],
-            resources: [`arn:aws:s3:::${bucket.bucketName}/uploads/*`],
-          }),
-          new iam.PolicyStatement({
-            actions: ['s3:GetObject'],
-            resources: [`arn:aws:s3:::${bucket.bucketName}/protected/*`],
-          }),
-          new iam.PolicyStatement({
-            actions: ['s3:ListBucket'],
-            resources: [`arn:aws:s3:::${bucket.bucketName}`],
-            conditions: {
-              StringLike: {
-                's3:prefix': [
-                  'public/',
-                  'public/*',
-                  'protected/',
-                  'protected/*',
-                  'private/${cognito-identity.amazonaws.com:sub}/',
-                  'private/${cognito-identity.amazonaws.com:sub}/*',
-                ],
-              },
+    //https://docs.amplify.aws/lib/storage/getting-started/q/platform/js/#using-amazon-s3
+    props.auth.identityPool.authenticatedRole.attachInlinePolicy(new iam.Policy(this, "authPolicy", {
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            's3:GetObject',
+            's3:PutObject',
+            's3:DeleteObject',
+          ],
+          resources: [
+            bucket.bucketArn + '/public/*',
+            bucket.bucketArn + '/protected/${cognito-identity.amazonaws.com:sub}/*',
+            bucket.bucketArn + '/private/${cognito-identity.amazonaws.com:sub}/*',
+          ],
+          effect: iam.Effect.ALLOW,
+        }),
+        new iam.PolicyStatement({
+          actions: ['s3:PutObject'],
+          resources: [`arn:aws:s3:::${bucket.bucketName}/uploads/*`],
+        }),
+        new iam.PolicyStatement({
+          actions: ['s3:GetObject'],
+          resources: [`arn:aws:s3:::${bucket.bucketName}/protected/*`],
+        }),
+        new iam.PolicyStatement({
+          actions: ['s3:ListBucket'],
+          resources: [`arn:aws:s3:::${bucket.bucketName}`],
+          conditions: {
+            StringLike: {
+              's3:prefix': [
+                'public/',
+                'public/*',
+                'protected/',
+                'protected/*',
+                'private/${cognito-identity.amazonaws.com:sub}/',
+                'private/${cognito-identity.amazonaws.com:sub}/*',
+              ],
             },
-          }),
-        ],
-      }))
+          },
+        }),
+      ],
+    }))
 
-      props.auth.identityPool.unauthenticatedRole.attachInlinePolicy(new iam.Policy(this, "unauthPolicy", {
-        statements: [
-          new iam.PolicyStatement({
-            actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
-            resources: [`${bucket.bucketArn}/public/*`],
-          }),
-          new iam.PolicyStatement({
-            actions: ['s3:PutObject'],
-            resources: [`${bucket.bucketArn}/uploads/*`],
-          }),
-          new iam.PolicyStatement({
-            actions: ['s3:GetObject'],
-            resources: [`${bucket.bucketArn}/protected/*`],
-          }),
-          new iam.PolicyStatement({
-            actions: ['s3:ListBucket'],
-            resources: [bucket.bucketArn],
-            conditions: {
-              StringLike: {
-                's3:prefix': [
-                  'public/',
-                  'public/*',
-                  'protected/',
-                  'protected/*',
-                ],
-              },
+    props.auth.identityPool.unauthenticatedRole.attachInlinePolicy(new iam.Policy(this, "unauthPolicy", {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
+          resources: [`${bucket.bucketArn}/public/*`],
+        }),
+        new iam.PolicyStatement({
+          actions: ['s3:PutObject'],
+          resources: [`${bucket.bucketArn}/uploads/*`],
+        }),
+        new iam.PolicyStatement({
+          actions: ['s3:GetObject'],
+          resources: [`${bucket.bucketArn}/protected/*`],
+        }),
+        new iam.PolicyStatement({
+          actions: ['s3:ListBucket'],
+          resources: [bucket.bucketArn],
+          conditions: {
+            StringLike: {
+              's3:prefix': [
+                'public/',
+                'public/*',
+                'protected/',
+                'protected/*',
+              ],
             },
-          }),
-        ],
-      }))
-    }
+          },
+        }),
+      ],
+    }))
+
 
     const vals: StorageVals = { bucket }
   }
