afc7bc4e4c13a934fe36cee2a922a3d4439a631e
Master__Harvey <ianpadraicharvey@gmail.com>
2023-06-02 13:34:25 -0600

    ini test stack

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Auth, Storage, Hosting } from 'camplify'
 
-export class TestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+//compiled during test phase
+export class TestStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const appName = "Test"
+        const Authentication = new Auth(this, "Authentication", { appName })
+        const AppStorage = new Storage(this, "AppStorage", { appName, auth: Authentication.vals })
+        //Other Camplify stacks as they're integrated
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // Your business logic infrastructure //
+
+        const FrontEnd = new Hosting(this, "Hosting", {
+            appName, repo: "Camplify", branch: "demo", gitOwner: "master-harvey",
+            URL: "camplifydemo.m-a.cloud",
+            buildEnvironment: { //Custom Env Vars passed to frontend
+                key1: { value: "value" },
+                key2: { value: "value" }
+            },
+            camplifyVals: {
+                AuthVals: Authentication.vals,
+                StorageVals: AppStorage.vals
+            }
+        })
+    }
 }
