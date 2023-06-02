300b71edfa21d1d6db58758a9b6da58352d1d4c3
Master__Harvey <ianpadraicharvey@gmail.com>
2023-06-02 14:36:46 -0600

    formatting env vars

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -4,27 +4,27 @@ import { Auth, Storage, Hosting } from 'camplify'
 
 //compiled during test phase
 export class TestStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const appName = "CamplifyTest"
-        const Authentication = new Auth(this, "Authentication", { appName })
-        const AppStorage = new Storage(this, "AppStorage", { appName, auth: Authentication.vals })
-        //Other Camplify stacks as they're integrated
+    const appName = "CamplifyTest"
+    const Authentication = new Auth(this, "Authentication", { appName })
+    const AppStorage = new Storage(this, "AppStorage", { appName, auth: Authentication.vals })
+    //Other Camplify stacks as they're integrated
 
-        // Your business logic infrastructure //
+    // Your business logic infrastructure //
 
-        const FrontEnd = new Hosting(this, "Hosting", {
-            appName, repo: "Camplify", branch: "demo", gitOwner: "master-harvey",
-            //URL: "camplifydemo.m-a.cloud",
-            buildEnvironment: { //Custom Env Vars passed to frontend
-                key1: { value: "value" },
-                key2: { value: "value" }
-            },
-            camplifyVals: {
-                AuthVals: Authentication.vals,
-                StorageVals: AppStorage.vals
-            }
-        })
-    }
+    const FrontEnd = new Hosting(this, "Hosting", {
+      appName, repo: "Camplify", branch: "demo", gitOwner: "master-harvey",
+      //URL: "camplifydemo.m-a.cloud",
+      buildEnvironment: { //Custom Env Vars passed to frontend
+        key1: "value",
+        key2: "value"
+      },
+      camplifyVals: {
+        AuthVals: Authentication.vals,
+        StorageVals: AppStorage.vals
+      }
+    })
+  }
 }
