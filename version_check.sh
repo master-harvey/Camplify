9d7930cef585ac6bc88e4b99a8d134de9fc7660d
Master__Harvey <ianpadraicharvey@gmail.com>
2023-06-06 04:06:37 -0600

    publish :/

--- version_check.sh
+++ version_check.sh
@@ -11,12 +11,12 @@ camplify_version=$(npm show camplify version)
 cdk_major=$(echo $cdk_version | cut -d. -f1)
 cdk_minor=$(echo $cdk_version | cut -d. -f2)
 cdk_patch=$(echo $cdk_version | cut -d. -f3)
-echo cdk_minor
+echo $cdk_minor
 
 camplify_major=$(echo $camplify_version | cut -d. -f1)
 camplify_minor=$(echo $camplify_version | cut -d. -f2)
 camplify_patch=$(echo $camplify_version | cut -d. -f3)
-echo camplify_minor
+echo $camplify_minor
 
 # Check if the major version of aws-cdk-lib is 3
 if [ $cdk_major -eq 3 ]; then
@@ -32,3 +32,4 @@ else
   new_version="${cdk_major}.${cdk_minor}.0"
   npm version $new_version
 fi
+npm publish
