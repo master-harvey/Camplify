df992e89bb79d7e0d43dc68bca69f157f547b2c1
Master__Harvey <ianpadraicharvey@gmail.com>
2023-06-06 23:51:43 -0600

    api integration w/ cors, doesn't verify repo name

--- lib/ci-stack.ts
+++ lib/ci-stack.ts
@@ -71,34 +71,59 @@ export class CamplifyCiStack extends Stack {
     // Version Pipeline \\
 
     //Create REST API
-    const api = new apigw.RestApi(this, "Market-Manager", {
+    const api = new apigw.RestApi(this, "Camplify-Version-Tracker", {
       restApiName: "Camplify-Version-Tracker",
-      description: "Manages instances of the Master's Market Environment",
+      description: "Triggers Camplify's codepipeline when a new CDK version is published",
       defaultCorsPreflightOptions: {
         allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key', 'X-Amz-Security-Token'],
-        allowOrigins: ['*'], allowCredentials: true, allowMethods: ['POST']
+        allowOrigins: ['https://libraries.io/'], allowCredentials: true, allowMethods: ['POST']
       }
     });
 
     //Api Model definitions
-    const modelRequest = api.addModel("startMarketRequestModel", {
+    const modelRequest = api.addModel("startPipelineRequestModel", {
       modelName: "startPipelineModelRequest",
       schema: {
+        required: ['name'],
         type: apigw.JsonSchemaType.OBJECT,
         properties: {
           "method.response.header.Access-Control-Allow-Origin": { type: apigw.JsonSchemaType.STRING },
-          "method.response.body.pipelineExecutionId": { type: apigw.JsonSchemaType.OBJECT }
-        },
-      },
+          "method.response.body": {
+            type: apigw.JsonSchemaType.OBJECT,
+            properties: {
+              event: { type: apigw.JsonSchemaType.STRING },
+              repository: { type: apigw.JsonSchemaType.STRING },
+              platform: { type: apigw.JsonSchemaType.STRING },
+              name: { type: apigw.JsonSchemaType.STRING, enum: ["aws-cdk"] },
+              version: { type: apigw.JsonSchemaType.STRING },
+              package_manager_url: { type: apigw.JsonSchemaType.STRING },
+              published_at: { type: apigw.JsonSchemaType.STRING },
+              project: {
+                type: apigw.JsonSchemaType.OBJECT,
+                properties: {
+                  name: { type: apigw.JsonSchemaType.STRING },
+                  platform: { type: apigw.JsonSchemaType.STRING },
+                  description: { type: apigw.JsonSchemaType.STRING },
+                  homepage: { type: apigw.JsonSchemaType.STRING },
+                  repository_url: { type: apigw.JsonSchemaType.STRING },
+                  normalized_licenses: { type: apigw.JsonSchemaType.ARRAY },
+                  latest_release_published_at: { type: apigw.JsonSchemaType.STRING },
+                  language: { type: apigw.JsonSchemaType.STRING },
+                },
+              }
+            }
+          }
+        }
+      }
     })
-    const modelResponse = api.addModel("startMarketResponseModel", {
+    const modelResponse = api.addModel("startPipelineResponseModel", {
       modelName: "startPipelineModelResponse",
       schema: {
         type: apigw.JsonSchemaType.OBJECT,
         properties: {
           "method.response.header.Access-Control-Allow-Origin": { type: apigw.JsonSchemaType.STRING },
           "method.response.body.pipelineExecutionId": { type: apigw.JsonSchemaType.OBJECT }
-        },
+        }
       }
     })
 
@@ -116,8 +141,7 @@ export class CamplifyCiStack extends Stack {
     const trackerResource = api.root.addResource('track')
     const requestValidator = api.addRequestValidator("validate-request", {
       requestValidatorName: "LibrariesIOvalidator",
-      validateRequestBody: true,
-      validateRequestParameters: true,
+      validateRequestBody: true, validateRequestParameters: true
     });
     const trackerMethod = trackerResource.addMethod('POST', new apigw.AwsIntegration({
       service: "codepipeline",
@@ -133,7 +157,9 @@ export class CamplifyCiStack extends Stack {
         requestTemplates: {
           "application/json": `{ "name":"${versionPipeline.pipelineName}" }`
         },
-        integrationResponses: [{ statusCode: '200', selectionPattern: '2\\d{2}' }],
+        integrationResponses: [
+          { statusCode: '200', selectionPattern: '2\\d{2}' }
+        ],
         passthroughBehavior: apigw.PassthroughBehavior.WHEN_NO_TEMPLATES
       }
     }),
