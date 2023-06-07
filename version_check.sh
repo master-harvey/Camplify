a2efba32a59edafc81bb404d9e33579e686b754d
Master__Harvey <ianpadraicharvey@gmail.com>
2023-06-07 00:37:05 -0600

    increase NPM version not the last one from github

--- version_check.sh
+++ version_check.sh
@@ -11,12 +11,12 @@ camplify_version=$(npm show camplify version)
 cdk_major=$(echo $cdk_version | cut -d. -f1)
 cdk_minor=$(echo $cdk_version | cut -d. -f2)
 cdk_patch=$(echo $cdk_version | cut -d. -f3)
-echo $cdk_minor
+echo "CDK Version" $cdk_major $cdk_minor $cdk_patch
 
 camplify_major=$(echo $camplify_version | cut -d. -f1)
 camplify_minor=$(echo $camplify_version | cut -d. -f2)
 camplify_patch=$(echo $camplify_version | cut -d. -f3)
-echo $camplify_minor
+echo "Camplify Version" $camplify_major $camplify_minor $camplify_patch
 
 # Check if the major version of aws-cdk-lib is 3
 if [ $cdk_major -eq 3 ]; then
@@ -26,7 +26,9 @@ fi
 # Check if the minor versions are equal
 if [ $cdk_minor -eq $camplify_minor ]; then
   # Increase the camplify patch version
-  npm version patch
+  new_patch=$(expr $camplify_patch + 1)
+  new_version="${cdk_major}.${cdk_minor}.${new_patch}"
+  npm version 
 else
   # Make the camplify minor version match aws-cdk-lib with patch number 0
   new_version="${cdk_major}.${cdk_minor}.0"
